19990cc343c75500ae2f4659a0e54d1b4110b527
Jackson Silva <jackson.silva.who@gmail.com>
2020-10-13 21:16:11 -0300

    orphanages map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; 
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
     return (
-        <h1>Hello World</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Maceió</strong>
+                    <span>Alagoas</span>
+                </footer>
+            </aside>
+
+            <Map 
+            center={[-9.6487357,-35.719373]}
+            zoom={15.38}
+            style={{ width:'100%', height:'100%' }}>
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff"/>
+            </Link>
+        </div>
     );
 }
 
