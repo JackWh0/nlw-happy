d6ddb3a1f04317239d5309d3ced1c4846e255413
Jackson Silva <jackson.silva.who@gmail.com>
2020-10-16 20:46:24 -0300

    Bug fix and images deleted

--- backend/src/views/images_view.ts
+++ backend/src/views/images_view.ts
@@ -4,7 +4,7 @@ export default {
     render(image: Image){
         return {
             id: image.id,
-            url: `http://localhost:3333/uploads/${image.path},`
+            url: `http://localhost:3333/uploads/${image.path}`
         };
     },
 
